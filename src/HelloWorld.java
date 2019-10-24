a403c9b721091f324f5d7825eb32d0ac55b0306c
Sreekumar <sreekumar@enterprisebot.org>
2019-10-24 09:41:50 +0530

    Add: Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * Hello world Java Program
+ */
+class HelloWorld 
+{ 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
